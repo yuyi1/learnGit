ffe96ef2a40034c18a24c618c8e39e678e17177e
yuyi1 <2472209307@qq.com>
2016-12-24 01:41:13 +0800

    11

---
+++ JqueryValidate_Demo/JqueryValidater/Test.ashx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JqueryValidater
+{
+    /// <summary>
+    /// Test 的摘要说明
+    /// </summary>
+    public class Test : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            var form = context.Request.Form;
+            var keys = form.Keys;
+            List<string> lists = new List<string>();
+
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
